ab46bd11727636b4a522a471dc9d758acca1c5d3
keke0123 <keke0123@naver.com>
2019-02-19 10:17:03 +0900

    vscode decorator error 처리 방법 commit

--- src/store/Counter.js
+++ src/store/Counter.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 
 import { observable, action } from 'mobx';
 
+// vscode 에서 decorator 설정법
+// ctrl+',' 누른후 decorators 검색후 체크 해주면 된다.
 export default class Counter{
     @observable number = 0;
 
